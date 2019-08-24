6ad8321ed271f05d07bf4366e99f8bf0f284a37a
cwern40 <chris.wernli@hotmail.com>
2019-08-24 11:44:23 -0600

    I created the post and get by ID requests

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,15 +4,47 @@ const db = require('../data/dbConfig')
 
 const router = express.Router();
 
+//Get request for list of cars /api/cars
 router.get('/', (req, res) => {
     db('cars')
         .then(cars => {
-            res.json(cars)
+            res.json(cars[0])
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({
                 message: "unable to retrieve cars"
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('cars').where({ id }).first()
+        .then(car => {
+            res.json(car)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "unable to retrieve car"
+            })
+        })
+})
+
+//post request to create a new car entry /api/cars
+router.post('/', (req, res) => {
+    const newCar = req.body
+
+    db('cars').insert(newCar)
+        .then(car => {
+            res.status(201).json(car)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "error creating new car"
             })
         })
 })
