f2f6f2cd072c0caab443f9f329c7d72fe252a821
cwern40 <chris.wernli@hotmail.com>
2019-08-24 12:04:57 -0600

    I created the delete endpoint to delet a car

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -67,4 +67,19 @@ router.put('/:id', (req, res) => {
         })
 })
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('cars').where({ id }).del()
+        .then(remove => {
+            res.json(remove)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "error deleting car"
+            })
+        })
+})
+
 module.exports = router;
